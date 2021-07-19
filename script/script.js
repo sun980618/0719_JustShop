57d04a69b8cf647af588724524f187f2ab7bd0cc
sun980618 <koarera98@gmail.com>
2021-07-19 13:59:09 +0900

    0719_JustShop

---
+++ script/script.js
@@ -0,0 +1,34 @@
+//eslint-disable
+
+$(function(){
+
+$(".mainLi"),mouseover(function(){
+    $(".subMenu").stop().slideDown(800);
+});
+
+
+$(".mainLi"),mouseout(function(){
+    $(".subMenu").stop().slideup(300);
+});
+
+setInterval
+
+$("#shuttleFrame").animate(
+    ${"maegin-top":"-300px"},
+    1000,
+    function(){
+$("#slide a:first-child")
+.insertAfter("#slide a:first-child");
+$("#shuttleFrame").css({"maegin-top":"0px"});
+    
+    });
+
+
+
+
+
+
+
+
+
+});
